377d00b195d1ee84f11c8da877e6645fbd474965
GabriPoli <gfo2@poli.br>
2024-09-17 20:35:59 -0300

    Add files via upload

--- Jogadores.java
+++ Jogadores.java
@@ -1,83 +1,74 @@
-package players;
-
-import cartas.Criatura;
-import enchantments.Vida;
+package MecanicasJogo;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
+import AtributosCartas.Criatura;
 public class Jogadores {
 
-    private String nomeJogador;
-    private int vidaJogador;
-    private int manaJogador;
-    private Criatura criatura;
-    private ArrayList<Criatura> criaturasLista;
-
-    public Jogadores(String nome, int vida, int mana) {
-
-        this.nomeJogador = nome;
-        this.vidaJogador = vida;
-        this.manaJogador = mana;
+	private String nomeJogador;
+	private int vidaJogador;
+	private int manaJogador;
+	private Criatura criatura;
+	private ArrayList<Criatura> mao;
+	
+	public Jogadores(String nome, int vida, int mana, ArrayList<Criatura> mao) {
+    	
+    	this.nomeJogador = nome;
+    	this.vidaJogador = vida;
+    	this.manaJogador = mana;
+    	this.mao = mao;
     }
+	
+	public void gastaMana(int manaGasta) {
+		manaJogador = manaJogador - manaGasta;
+	}
+	
+	public void perdeVida(int dano) {
+		vidaJogador = vidaJogador - dano;
+	}
+	
+	public void ganhaVida(int cura) {
+		vidaJogador = vidaJogador + cura;
+	}
+	
+	public int getVidaJogador() {
+		return vidaJogador;
+	}
+	public int getManaJogador() {
+		return manaJogador;
+	}
 
-    public void gastaMana(int manaGasta) {
-        manaJogador = manaJogador - manaGasta;
-    }
+	public String getNomeJogador() {
+		return nomeJogador;
+	}
 
-    public void perdeVida(int dano) {
-        vidaJogador = vidaJogador - dano;
-    }
+	public void setNomeJogador(String nomeJogador) {
+		this.nomeJogador = nomeJogador;
+	}
 
-    public void ganhaVida(int cura) {
-        vidaJogador = vidaJogador + cura;
-    }
+	public void setVidaJogador(int vidaJogador) {
+		this.vidaJogador = vidaJogador;
+	}
 
-    public int getVidaJogador() {
-        return vidaJogador;
-    }
-    public int getManaJogador() {
-        return manaJogador;
-    }
+	public void setManaJogador(int manaJogador) {
+		this.manaJogador = manaJogador;
+	}
 
-    public String getNomeJogador() {
-        return nomeJogador;
-    }
+	public Criatura getCriatura() {
+		return criatura;
+	}
 
-    public void setNomeJogador(String nomeJogador) {
-        this.nomeJogador = nomeJogador;
-    }
+	public void setCriatura(Criatura criatura) {
+		this.criatura = criatura;
+	}
 
-    public void setVidaJogador(int vidaJogador) {
-        this.vidaJogador = vidaJogador;
-    }
+	public ArrayList<Criatura> getMao() {
+		return mao;
+	}
 
-    public void setManaJogador(int manaJogador) {
-        this.manaJogador = manaJogador;
-    }
-
-    public Criatura getCriatura() {
-        return criatura;
-    }
-
-    public void setCriatura(Criatura criatura) {
-        this.criatura = criatura;
-    }
-
-    public ArrayList<Criatura> getCriaturasLista() {
-        return criaturasLista;
-    }
-
-    public void setCriaturasLista(ArrayList<Criatura> criaturasLista) {
-        this.criaturasLista = criaturasLista;
-    }
-
-    //Método Para o Encantamento
-    public void aumentarVida(int quantidade) {
-        vidaJogador = vidaJogador + quantidade;
-    }
-
-    public void aumentarMana(int quantidade) {
-        manaJogador = manaJogador + quantidade;
-    }
+	public void setMao(ArrayList<Criatura> mao) {
+		this.mao = mao;
+	}
+	
+	
 }
--- Jogo.java
+++ Jogo.java
@@ -1,139 +1,140 @@
-package mecanicasjogo;
+package MecanicasJogo;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import cartas.Carta;
-import criaturas.Cerbero;
-import criaturas.Fenix;
-import criaturas.Golem;
-import players.Jogadores;
-import cartas.Criatura;
-import enchantments.*;
+import AtributosCartas.*;
 
 public class Jogo {
 
-    private int turno = 1;
-    private boolean isJogador1 = true;
+	private int turno = 1;
+	private boolean isJogador1 = true;
+	private ArrayList<Criatura> listaCriaturas1;
+	private ArrayList<Criatura> listaCriaturas2;
+	
+	public Jogo() {
 
-    //Instanciando os Encantamentos
-    Vida vida = new Vida();
-    Mana mana = new Mana();
+	}
+	
+	public void cartasDaMao(Jogadores maoJogador) {
+		
+		Cerbero cerbero = new Cerbero();
+        Fenix fenix = new Fenix();
+        Golem golem = new Golem();
+		Minotauro minotauro = new Minotauro();
+		Grifo grifo = new Grifo();
+		Yeti yeti = new Yeti();
+        
+		if(isJogador1) {
+			listaCriaturas1 = maoJogador.getMao();
+			System.out.println("Mão de " + maoJogador.getNomeJogador());
+			listaCriaturas1 = new ArrayList<Criatura>();
+			listaCriaturas1.add(cerbero);
+			listaCriaturas1.add(grifo);
+			listaCriaturas1.add(minotauro);
+			
+			for(int i = 0; i < listaCriaturas1.size(); i++) {
+				System.out.println((i + 1) + " - " + listaCriaturas1.get(i).getNome());
+			}
+		}
+		else {
+			listaCriaturas2 = maoJogador.getMao();
+			System.out.println("Mão de " + maoJogador.getNomeJogador());
+			listaCriaturas2 = new ArrayList<Criatura>();
+			listaCriaturas2.add(fenix);
+			listaCriaturas2.add(golem);
+			listaCriaturas2.add(yeti);
+			
+			for(int i = 0; i < listaCriaturas2.size(); i++) {
+				System.out.println((i + 1) + " - " + listaCriaturas2.get(i).getNome());
+			}
+		}
+		
+	}
+	
+	public void comecoJogo(Jogadores jogador1, Jogadores jogador2) {
 
-    //Instanciando as Criaturas
-    Cerbero cerbero = new Cerbero();
-    Fenix fenix = new Fenix();
-    Golem golem = new Golem();
+		Scanner escolhaJogador1 = new Scanner(System.in);
+		
+		cartasDaMao(jogador1);
+		System.out.println(jogador1.getNomeJogador() +", digite o nome da criatura que deseja usar: ");
+		int criatura1 = escolhaJogador1.nextInt();
+		Criatura criaturaJogador1 = listaCriaturas1.get(criatura1 - 1);
+		jogador1.setCriatura(criaturaJogador1);
+		
+		isJogador1 = false;
+				
+		Scanner escolhaJogador2 = new Scanner(System.in);
+		
+		cartasDaMao(jogador2);
+		System.out.println(jogador2.getNomeJogador() +", digite o nome da criatura que deseja usar: ");
+		int criatura2 = escolhaJogador2.nextInt();
+		Criatura criaturaJogador2 = listaCriaturas2.get(criatura2 - 1);
+		jogador2.setCriatura(criaturaJogador2);
+	}
 
-    //ArrayList para o Deck
-    ArrayList<Criatura> deck = new ArrayList<Criatura>();
+	public void exibirStatus(Jogadores jogador1, Jogadores jogador2) {
+		System.out.println("===== Status Atual =====");
+		System.out.println("Vida do Jogador 1: " + jogador1.getVidaJogador());
+		System.out.println("Vida do Jogador 2: " + jogador2.getVidaJogador());
+		System.out.println("========================");
+	}
 
+	public void executarTurno(int acao1, Jogadores jogador1, Jogadores jogador2) {
 
-    public Jogo() {
+		System.out.println("Turno " + turno);
 
-    }
+		if (acao1 == 1) {
+			
+			if(isJogador1) {
+				cartasDaMao(jogador1);
+				atacar(jogador1, jogador2);
+				isJogador1 = false;
+			}
+			else {
+				cartasDaMao(jogador2);
+				atacar(jogador2, jogador1);
+				isJogador1 = true;
+			}
+		}
+		
+		System.out.println("Fim do truno " + turno);
+		turno++;
+	}
 
-    //Método para o Jogador1 escolher o seu encantamento
-    public void escolherEncantamentoJogador1(Jogadores jogador1) {
-        System.out.println(jogador1.getNomeJogador() + " Escolha o seu encantamento:  ");
-        System.out.println("1. Vida\n2. Mana");
-        Scanner escolha = new Scanner(System.in);
-        int decisao = escolha.nextInt();
+	public void atacar(Jogadores jogadorAtaque, Jogadores jogadorDefesa) {
 
-        switch(decisao) {
-            case 1:
-                vida.ganharVida(jogador1);
-                System.out.println("Você escolheu o encantamento: " + vida.getNome() + "  Seu novo HP: " + jogador1.getVidaJogador());
-                break;
-            case 2:
-                mana.ganharMana(jogador1);
-                System.out.println("Você escolheu o encantamento: " + mana.getNome() + "  Sua nova Mana: " + jogador1.getManaJogador());
-                break;
-        }
-    }
+		if (jogadorDefesa.getCriatura().getResistencia() < jogadorAtaque.getCriatura().getPoder()) {
 
-    //Método para o Jogador2 escolher o seu encantamento
-    public void escolherEncantamentoJogador2(Jogadores jogador2) {
+			int dano = jogadorAtaque.getCriatura().getPoder() - jogadorDefesa.getCriatura().getResistencia();
+			int vidaAtual = jogadorDefesa.getVidaJogador() - dano;
+			jogadorDefesa.getCriatura().setVivo(false);
+			jogadorDefesa.setVidaJogador(vidaAtual);
+		} else {
 
-        System.out.println(jogador2.getNomeJogador() + " Escolha o seu encantamento:  ");
-        System.out.println("1. Vida\n2. Mana");
-        Scanner escolha = new Scanner(System.in);
-        int decisao = escolha.nextInt();
+			int vidaAtual = jogadorDefesa.getVidaJogador();
+			jogadorDefesa.getCriatura().setVivo(true);
+			jogadorDefesa.setVidaJogador(vidaAtual);
+		}
+	}
 
-        switch(decisao) {
-            case 1:
-                vida.ganharVida(jogador2);
-                System.out.println("Você escolheu o encantamento: " + vida.getNome() + "  Seu novo HP: " + jogador2.getVidaJogador());
-                break;
-            case 2:
-                mana.ganharMana(jogador2);
-                System.out.println("Você escolheu o encantamento: " + mana.getNome() + "  Sua nova Mana: " + jogador2.getManaJogador());
-                break;
-        }
+	public ArrayList<Criatura> getListaCriaturas1() {
+		return listaCriaturas1;
+	}
 
-    }
+	public ArrayList<Criatura> getListaCriaturas2() {
+		return listaCriaturas2;
+	}
+	
+	
 
-    public void comecoJogo(Jogadores jogador1, Jogadores jogador2) {
+	//public void cartasDisponiveis(Jogadores maoJogador1, Jogadores maoJogador2) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println(jogador1.getCriatura() +", digite o nome da criatura que deseja usar: ");
-        String nomeCriatura1 = input.next();
-        nomeCriatura1 = jogador1.getCriatura().getNome();
-        System.out.println(jogador2.getNomeJogador() +", digite o nome da criatura que deseja usar: ");
-        String nomeCriatura2 = input.next();
-        nomeCriatura2 = jogador2.getCriatura().getNome();
-    }
+		//ArrayList<Criatura> listaCriaturas1 = new ArrayList<Criatura>();
+		//listaCriaturas1.add(maoJogador1.getCriatura().getNome());
 
-    public void exibirStatus(Jogadores jogador1, Jogadores jogador2) {
-        System.out.println("===== Status Atual =====");
-        System.out.println("Vida do Jogador 1: " + jogador1.getVidaJogador());
-        System.out.println("Vida do Jogador 2: " + jogador2.getVidaJogador());
-        System.out.println("========================");
-    }
+		//ArrayList<Criatura> listaCriaturas2 = maoJogador2.getCriaturasLista();
 
-    public void executarTurno(int acao1, Jogadores jogador1, Jogadores jogador2) {
-
-        System.out.println("Turno " + turno);
-
-        if (acao1 == 1) {
-
-            if(isJogador1) {
-                atacar(jogador1, jogador2);
-                isJogador1 = false;
-            }
-            else {
-                atacar(jogador2, jogador1);
-                isJogador1 = true;
-            }
-        }
-
-        System.out.println("Fim do truno " + turno);
-        turno++;
-    }
-
-    public void atacar(Jogadores jogadorAtaque, Jogadores jogadorDefesa) {
-
-        if (jogadorDefesa.getCriatura().getResistencia() < jogadorAtaque.getCriatura().getPoder()) {
-
-            int dano = jogadorAtaque.getCriatura().getPoder() - jogadorDefesa.getCriatura().getResistencia();
-            int vidaAtual = jogadorDefesa.getVidaJogador() - dano;
-            jogadorDefesa.getCriatura().setVivo(false);
-            jogadorDefesa.setVidaJogador(vidaAtual);
-        } else {
-
-            int vidaAtual = jogadorDefesa.getVidaJogador();
-            jogadorDefesa.getCriatura().setVivo(true);
-            jogadorDefesa.setVidaJogador(vidaAtual);
-        }
-    }
-
-//public void cartasDisponiveis(Jogadores maoJogador1, Jogadores maoJogador2) {
-
-//ArrayList<Criatura> listaCriaturas1 = new ArrayList<Criatura>();
-//listaCriaturas1.add(maoJogador1.getCriatura().getNome());
-
-//ArrayList<Criatura> listaCriaturas2 = maoJogador2.getCriaturasLista();
-
-//}
+	//}
 
 }
-
